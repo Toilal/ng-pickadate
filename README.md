9b290ea2e12f66ff92236442bad75e77bc567701
Toilal <toilal.dev@gmail.com>
2014-12-16 11:13:31 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,40 @@
+ng-pickadate
+====================
+
+Set of AngularJS directives for [pickadate.js by Amsul](http://amsul.ca/pickadate.js/).
+
+### Requirements
+
+- [AngularJS](https://angularjs.org/)
+- [pickadate](http://amsul.ca/pickadate.js/)
+- [JQuery](http://jquery.com/)
+
+### Install
+
+    bower install ng-pickadate
+
+### Usage
+
+1. Declare the dependency
+
+        angular.module('yourApp', [pickadate]);
+
+2. Use `pick-a-date` and `pick-a-time` directives.
+
+        <input type="text" pick-a-date="curDate"/>
+        <input type="text" pick-a-time="curDate"/>
+
+3. You can also provide additionnal `max-date` and `min-date` values.
+
+        <input type="text" pick-a-date="startDate" max-date="endDate"/>
+        <input type="text" pick-a-date="endDate" min-date="startDate"/>
+
+### Options
+
+You can define [pickadate.js](http://amsul.ca/pickadate.js/) options through `pick-a-date-options` and `pick-a-time-options` directives as well.
+
+    <input type="text" pick-a-date="curDate" pick-a-date-options="{ format: 'dd/mm/yy', selectYears: true }" />
+
+### Credits
+
+This project is initially based on a [blog post from Coding Insight](http://www.codinginsight.com/angularjs-and-pickadate/)
---
+++ ng-pickadate.js
@@ -0,0 +1,122 @@
+'use strict';
+
+angular.module('pickadate', []);
+
+angular.module('pickadate').directive('pickADate', function () {
+    return {
+        restrict: 'A',
+        scope: {
+            pickADate: '=',
+            minDate: '=',
+            maxDate: '=',
+            pickADateOptions: '='
+        },
+        link: function (scope, element) {
+            var options = angular.extend(scope.pickADateOptions || {}, {
+                onSet: function (e) {
+                    if (scope.$$phase || scope.$root.$$phase) { // we are coming from $watch or link setup
+                        return;
+                    }
+                    var select = element.pickadate('picker').get('select'); // selected date
+                    scope.$apply(function () {
+                        if (e.hasOwnProperty('clear')) {
+                            scope.pickADate = null;
+                            return;
+                        }
+                        if (!scope.pickADate) {
+                            scope.pickADate = new Date(0);
+                        }
+                        scope.pickADate.setYear(select.obj.getFullYear());
+                        scope.pickADate.setMonth(select.obj.getMonth());
+                        scope.pickADate.setDate(select.obj.getDate());
+                    });
+                },
+                onClose: function () {
+                    element.blur();
+                }
+            });
+            element.pickadate(options);
+            function updateValue(newValue) {
+                if (newValue) {
+                    scope.pickADate = (newValue instanceof Date) ? newValue : new Date(newValue);
+                    element.pickadate('picker').set('select', scope.pickADate.getTime());
+                } else {
+                    element.pickadate('picker').clear();
+                    scope.pickADate = null;
+                }
+            }
+
+            updateValue(scope.pickADate);
+            element.pickadate('picker').set('min', scope.minDate ? scope.minDate : false);
+            element.pickadate('picker').set('max', scope.maxDate ? scope.maxDate : false);
+            scope.$watch('pickADate', function (newValue, oldValue) {
+                if (newValue === oldValue) {
+                    return;
+                }
+                updateValue(newValue);
+            }, true);
+            scope.$watch('minDate', function (newValue) {
+                element.pickadate('picker').set('min', newValue ? newValue : false);
+            }, true);
+            scope.$watch('maxDate', function (newValue) {
+                element.pickadate('picker').set('max', newValue ? newValue : false);
+            }, true);
+        }
+    };
+});
+
+angular.module('pickadate').directive('pickATime', function () {
+    return {
+        restrict: 'A',
+        scope: {
+            pickATime: '=',
+            pickATimeOptions: '='
+        },
+        link: function (scope, element) {
+            var options = angular.extend(scope.pickATimeOptions || {}, {
+                onSet: function (e) {
+                    if (scope.$$phase || scope.$root.$$phase) { // we are coming from $watch or link setup
+                        return;
+                    }
+                    var select = element.pickatime('picker').get('select'); // selected date
+                    scope.$apply(function () {
+                        if (e.hasOwnProperty('clear')) {
+                            scope.pickATime = null;
+                            return;
+                        }
+                        if (!scope.pickATime) {
+                            scope.pickATime = new Date(0);
+                        }
+                        scope.pickATime.setHours(select.hour);
+                        scope.pickATime.setMinutes(select.mins);
+                        scope.pickATime.setSeconds(0);
+                        scope.pickATime.setMilliseconds(0);
+                    });
+                },
+                onClose: function () {
+                    element.blur();
+                }
+            });
+            element.pickatime(options);
+            function updateValue(newValue) {
+                if (newValue) {
+                    scope.pickATime = (newValue instanceof Date) ? newValue : new Date(newValue);
+                    var totalMins = scope.pickATime.getHours() * 60 + scope.pickATime.getMinutes();
+                    element.pickatime('picker').set('select', totalMins);
+                } else {
+                    element.pickatime('picker').clear();
+                    scope.pickATime = null;
+                }
+            }
+
+            updateValue(scope.pickATime);
+            scope.$watch('pickATime', function (newValue, oldValue) {
+                if (newValue === oldValue) {
+                    return;
+                }
+                updateValue(newValue);
+            }, true);
+        }
+    };
+});
+
